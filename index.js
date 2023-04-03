f83f13b29ee7267fc071f598d37c1f69a02ddcb8
shefinannathampy <shefin04annathampy94@gmail.com>
2023-04-02 21:52:08 -0400

    filter lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  function fuzzyMatch(drivers, letter) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letter.toLowerCase()));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
